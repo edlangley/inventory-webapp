deceddfde98a516505ac1de497727371a0d1070e
Ed Langley <ed@edlangley.co.uk>
2013-08-12 09:20:22 +0100

    Code beautifying take 2.

--- www-src/index.php
+++ www-src/index.php
@@ -1,15 +1,20 @@
 <?php
-        include("site_title.php");
+        include("site-title.php");
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
-<head>
-<meta name="generator" content=
-"HTML Tidy for Linux/x86 (vers 25 March 2009), see www.w3.org">
-<title><?php echo $site_title; ?></title>
-</head>
-<frameset cols="15%, 85%" border="0">
-<frame src="sidebar.php" scrolling="no">
-<frame src="view-items.php" name="main">
-</frameset>
+        <head>
+                <meta name="generator"
+                      content="HTML Tidy for Linux/x86 (vers 25 March 2009), see www.w3.org">
+                <title>
+                        <?php echo $site_title; ?>
+                </title>
+        </head>
+        <frameset cols="15%, 85%"
+                  border="0">
+                <frame src="sidebar.php"
+                       scrolling="no">
+                <frame src="view-items.php"
+                       name="main">
+        </frameset>
 </html>
--- www-src/move-item.php
+++ www-src/move-item.php
@@ -1,33 +1,35 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
-<head>
-<meta name="generator" content=
-"HTML Tidy for Linux/x86 (vers 25 March 2009), see www.w3.org">
-<title></title>
-</head>
-<body>
-<?php
-        include("site_title.php");
-        include("admin-auth.php");
-    // database is now selected and connected - index is $conn
-?>
-<h1><?php echo $site_title; ?></h1>
-<hr>
-<?php
-    $item_id = $_GET["item_id"];
-    $moveto_cat_id = $_GET["category"];
-    //echo "$moveto_cat_id";
-    $moveitemquery = "UPDATE item SET cat_id = '".$moveto_cat_id."' WHERE id = '"
-    .$item_id."';";
-    if(mysql_query($moveitemquery, $conn))
-    {
-        echo "Item successfully moved.";
-    }
-    else
-    {
-        echo "Error, item was not moved.";
-    }
-    echo "<br><a href='view-items.php'>Back to item list</a>";
-?>
-</body>
+        <head>
+                <meta name="generator"
+                      content="HTML Tidy for Linux/x86 (vers 25 March 2009), see www.w3.org">
+                <title></title>
+        </head>
+        <body>
+                <?php
+                        include("site-title.php");
+                        include("admin-auth.php");
+                    // database is now selected and connected - index is $conn
+                ?>
+                <h1>
+                        <?php echo $site_title; ?>
+                </h1>
+                <hr>
+                <?php
+                    $item_id = $_GET["item_id"];
+                    $moveto_cat_id = $_GET["category"];
+                    //echo "$moveto_cat_id";
+                    $moveitemquery = "UPDATE item SET cat_id = '".$moveto_cat_id."' WHERE id = '"
+                    .$item_id."';";
+                    if(mysql_query($moveitemquery, $conn))
+                    {
+                        echo "Item successfully moved.";
+                    }
+                    else
+                    {
+                        echo "Error, item was not moved.";
+                    }
+                    echo "<br><a href='view-items.php'>Back to item list</a>";
+                ?>
+        </body>
 </html>
